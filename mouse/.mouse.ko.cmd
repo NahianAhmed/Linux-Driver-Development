116f6fb0bdbc96e0c8cf0b0670958d231b1930b4
Nahian Ahmed <mailingtonahian@gmail>
2020-01-14 10:21:11 +0600

    adding mouse.c file

---
+++ mouse/.mouse.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/nahian/Desktop/ldd/mouse/mouse.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/nahian/Desktop/ldd/mouse/mouse.ko /home/nahian/Desktop/ldd/mouse/mouse.o /home/nahian/Desktop/ldd/mouse/mouse.mod.o ;  true
---
+++ mouse/.mouse.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/nahian/Desktop/ldd/mouse/mouse.mod := { echo  /home/nahian/Desktop/ldd/mouse/mouse.o; echo; } > /home/nahian/Desktop/ldd/mouse/mouse.mod
